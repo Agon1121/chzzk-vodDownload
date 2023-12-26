0ae1ae3c6bd29094d6632813f22a617651139ce0
Agon1121 <qqq0www2@gmail.com>
2023-12-26 21:20:04 +0900

    first commit

--- requesting.js
+++ requesting.js
@@ -15,15 +15,13 @@ request(options, function (error, response, body) {
     if (error) throw new Error(error);
     const parser = new DOMParser();
     const xmlData = parser.parseFromString(body, "text/xml");
-
-    // XML 데이터를 문자열로 변환
     const xmlString = xmlData.toString();
-
-    // XML 데이터를 파일로 저장
     const data = JSON.parse(xmlString);
 
     data.period[0].adaptationSet[0].representation.forEach(function (item) {
-        console.log(`(${item.any[0].value}fps, ${item.any[1].value}p)\n${item.baseURL[0].value}\n`)
-    })
+      console.log(
+        `(${item.any[0].value}fps, ${item.any[1].value}p)\n${item.baseURL[0].value}\n`
+      );
+    });
   });
 });
