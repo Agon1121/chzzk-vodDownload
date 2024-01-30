d60ff39256dd7ce380665b2eeb7aba0454738349
Agon1121 <qqq0www2@gmail.com>
2024-01-30 11:30:31 +0900

    all edited

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# 준비물
-1. nodeJS
+## REQUIREMENTS
+---
+1. Download [NodeJS](https://nodejs.org/dist/v20.11.0/node-v20.11.0-x64.msi)
+2. run code 
+```
+npm run p VODlink
+```
+
+※ You can't download age-restricted VODs.
--- requesting.js
+++ requesting.js
@@ -1,26 +1,17 @@
-const request = require("request");
-const DOMParser = require("xmldom").DOMParser;
 
-var vodLinkValue = "https://chzzk.naver.com/video/60644"
+var vodLinkValue = process.argv[2]
 var serialNum = vodLinkValue.replace("https://chzzk.naver.com/video/","");
-console.log(serialNum);
+var APIurl = `https://api.chzzk.naver.com/service/v2/videos/${serialNum}`
 const options = {
-  method: "GET",
-  url: `https://api.chzzk.naver.com/service/v2/videos/${serialNum}`,
+  method: "GET"
 };
-
-request(options, function (error, response, body) {
-  if (error) throw new Error(error);
-  const data = JSON.parse(body);
-  const xmlLink = `https://apis.naver.com/neonplayer/vodplay/v2/playback/${data.content.videoId}?key=${data.content.inKey}&env=real`;
-
-  request(xmlLink, function (error, response, body) {
-    if (error) throw new Error(error);
-    const parser = new DOMParser();
-    const xmlData = parser.parseFromString(body, "text/xml");
-    const xmlString = xmlData.toString();
-    const data = JSON.parse(xmlString);
-
+fetch(APIurl, options)
+.then(response => response.json())
+.then((data) => {
+  var xmlLink = `https://apis.naver.com/neonplayer/vodplay/v2/playback/${data.content.videoId}?key=${data.content.inKey}&env=real`;
+  fetch(xmlLink, options)
+  .then(response => response.json())
+  .then(data => {
     data.period[0].adaptationSet[0].representation.forEach(function (item) {
       console.log(
         `(${item.any[0].value}fps, ${item.any[1].value}p)\n${item.baseURL[0].value}\n`
